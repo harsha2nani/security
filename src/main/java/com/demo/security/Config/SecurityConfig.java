94a37c19038bf7ad6b686adcaeabf3c50241be4b
harsha2nani <harshadeveloper.999@gmail.com>
2024-10-08 21:08:29 +0530

    jwtImpl

--- src/main/java/com/demo/security/Config/SecurityConfig.java
+++ src/main/java/com/demo/security/Config/SecurityConfig.java
@@ -1,18 +1,22 @@
 package com.demo.security.Config;
 
+import com.demo.security.Filter.JwtAuthFilter;
 import com.demo.security.Utills.SecurityUtills;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +24,8 @@ public class SecurityConfig {
 
     @Autowired
     private SecurityUtills securityUtills;
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     /**
      *
@@ -37,9 +43,15 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .authenticationProvider(authenticationProvider())
+               .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 
     /**
      *
--- src/main/java/com/demo/security/Controller/AuthController.java
+++ src/main/java/com/demo/security/Controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.demo.security.Controller;
 
+import com.demo.security.DTO.UserDto;
 import com.demo.security.Model.UserInfo;
 import com.demo.security.Service.AuthService;
+import com.demo.security.Service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +22,10 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     /**
      *
@@ -26,5 +36,20 @@ public class AuthController {
     public ResponseEntity<UserInfo> register(@RequestBody UserInfo userInfo){
         UserInfo userInfo1 = authService.register(userInfo);
         return new ResponseEntity<>(userInfo1, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public String authenticateAndValidateToken(@RequestBody UserDto userDto){
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userDto.getName(),userDto.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(userDto.getName());
+        }else{
+            throw new UsernameNotFoundException("Invalid User");
+        }
+
+
     }
 }
